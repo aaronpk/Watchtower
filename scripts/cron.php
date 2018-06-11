ece24b0191023fb0d0ede1a0245ee1983fb42741
Aaron Parecki <aaron@parecki.com>
2018-06-11 19:09:17 +0000

    only check feeds with active subscribers. really this is a hack fix for the underlying problem of apparently feeds are not being deleted when the last subscriber is removed.

--- scripts/cron.php
+++ scripts/cron.php
@@ -4,6 +4,7 @@ include('vendor/autoload.php');
 
 // Check the list of feeds and queue jobs for any that are due for an update
 $feeds = ORM::for_table('feeds')
+  ->join('subscribers', ['feeds.id','=','subscribers.feed_id'])
   ->where_lt('next_check_at', date('Y-m-d H:i:s'))
   ->find_many();
 foreach($feeds as $feed) {
