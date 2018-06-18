fb3a6233b57bf9bbb824730ce73f2b62960a3795
Aaron Parecki <aaron@parecki.com>
2018-06-18 12:48:44 +0000

    fix scheduling job

--- scripts/cron.php
+++ scripts/cron.php
@@ -4,9 +4,11 @@ include('vendor/autoload.php');
 
 // Check the list of feeds and queue jobs for any that are due for an update
 $feeds = ORM::for_table('feeds')
+  ->select_many_expr('feeds.*')
   ->join('subscribers', ['feeds.id','=','subscribers.feed_id'])
   ->where_lt('next_check_at', date('Y-m-d H:i:s'))
   ->find_many();
 foreach($feeds as $feed) {
+  echo "Queuing $feed->id $feed->url\n";
   q()->queue('Jobs\\CheckFeed', 'poll', $feed->id);
 }
